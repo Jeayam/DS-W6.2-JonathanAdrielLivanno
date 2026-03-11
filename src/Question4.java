38a46b1bde124101ebd17d37837e8090c44ac70d
Jeayam <jlivanno@student.ciputra.ac.id>
2026-03-11 22:16:47 +0800

    question 4 file

---
+++ src/Question4.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Question4 {
+    public static void main(String[] args) throws Exception {
+        
+    }}
